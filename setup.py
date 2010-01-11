035e6030c0c30c433018ed2ce3c3a4094f0956f2
Michael Sanders <msanders42@gmail.com>
2010-01-11 10:21:24 -0500

    version number changed to PEP 386-style. closes issue #6.

--- setup.py
+++ setup.py
@@ -228,7 +228,7 @@ else:
     DATA_FILES = []
 
 setup(name=PACKAGE_NAME,
-      version='0.43 beta',
+      version='0.43b',
       author='Michael Sanders',
       author_email='msanders42+autopy [at] gmail [dot] com',
       description=PACKAGE_DESCRIPTION,
