829c8798d5cc81d54371c2fe277519f6e12e4df8
Michael Sanders <msanders42@gmail.com>
2011-05-30 09:18:52 -0400

    Removed trailing whitespace.

--- autopy/__init__.py
+++ autopy/__init__.py
@@ -1,7 +1,7 @@
 """
 AutoPy is a simple, cross-platform GUI automation toolkit for Python. It
 includes functions for controlling the keyboard and mouse, finding colors and
-bitmaps on screen, and displaying alerts -- all in a cross-platform, efficient,
+bitmaps on-screen, and displaying alerts -- all in a cross-platform, efficient,
 and simple manner.
 """
 
--- setup.py
+++ setup.py
@@ -216,8 +216,8 @@ def create_ext_modules(src_dir):
 PACKAGE_NAME = 'autopy'
 PACKAGE_DESCRIPTION = \
 '''AutoPy is a simple, cross-platform GUI automation toolkit for Python. It
-includes functions for controlling the keyboard and mouse, finding colors 
-and bitmaps on-screen, and displaying alerts -- all in a cross-platform, 
+includes functions for controlling the keyboard and mouse, finding colors
+and bitmaps on-screen, and displaying alerts -- all in a cross-platform,
 efficient, and simple manner.'''
 EXT_MODULES = create_ext_modules('src/')
 
